38284e9a6c6e4e14b33332b01490ea7111667b07
ahad-070 <ahad.mardofi@thekaustschool.org>
2025-04-27 13:48:49 +0300

    Update U2 Selection.py

--- U2 Selection.py
+++ U2 Selection.py
@@ -5,7 +5,23 @@ name = str(input("What is your name?: "))
 print("Welcome to the SDGs Trivia Game name,where you will get to learn more about the goals,become more aware,and gain valuable infromation to get in action for helping achieve the goals?")
 
 #List of the questions
-"Which SDG goal relates to agriculture?"
-"Which SDG goal relates to hospitals?"
-"Which SDG goal relates to having a fair life"
-"Which SDG goal relates to marine pollution?"
+questions = ["Which SDG goal relates to agriculture?", "Which SDG goal relates to hospitals?", "Which SDG goal relates to having a fair life?","Which SDG goal relates to marine pollution?"]
+answers = ["Zero Hunger", "Good Health and well-being", "Quality Education","Life Below Water"]
+
+#List of hints
+q1=["Sustainable agriculture practices increase food resilience in climate-vulnerable regions.,Malnutrition remains a silent crisis, often rooted in poverty and unequal food distribution."]
+q2=["Mental health is often excluded from public health discussions despite rising global demand.,Preventable diseases continue to thrive in communities with weak infrastructure and low trust in institutions."]
+q3=["Gender disparities in education are often intensified by cultural norms and conflict.,Education reform must go beyond access ,it must also ensure relevance, quality, and inclusion."]
+q4=["Ocean ecosystems are collapsing under the weight of industrial overfishing and pollution.,Coral bleaching serves as a visible indicator of wider climate instability."]
+
+
+
+
+#for i in range(0,3):
+   # ans = input(questions[i])
+   # if ans == answers[i]:
+    #    print("well done")
+
+
+
+
