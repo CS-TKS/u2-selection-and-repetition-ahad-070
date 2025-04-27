efbc20afb976d2dfe414fcbd19fbf6134ff0f000
ahad-070 <ahad.mardofi@thekaustschool.org>
2025-04-27 12:16:29 +0300

    Create U2 Selection.py

---
+++ U2 Selection.py
@@ -0,0 +1,11 @@
+#SDGS Trivia Game
+
+#Get the user name and introduce the game
+name = str(input("What is your name?: "))
+print("Welcome to the SDGs Trivia Game name,where you will get to learn more about the goals,become more aware,and gain valuable infromation to get in action for helping achieve the goals?")
+
+#List of the questions
+"Which SDG goal relates to agriculture?"
+"Which SDG goal relates to hospitals?"
+"Which SDG goal relates to having a fair life"
+"Which SDG goal relates to marine pollution?"
